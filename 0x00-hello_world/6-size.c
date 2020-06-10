13fc312a5d4f1a4e2cf1857611e828d054d04542
Ivan E. Batista-Ochoa <1835@holbertonschool.com>
2020-06-10 20:40:59 +0000

    Mandatory task

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,17 +1,22 @@
 #include<stdio.h>
-int main() {
-  char charType;
-  int intType;
-  float floatType;
-  long int longIntType;
-  long long int longLongType;
+/**
+ *main - Entry point
+ *Return: Always 0 (Success)
+ */
 
-  printf("Size of char: %ld byte(s)\n", sizeof(charType));
-  printf("Size of int: %ld bytes(s)\n", sizeof(intType));
-  printf("Size of long int: %li byte(s)\n", sizeof(longIntType));
-  printf("Size of long long int: %ld byte(s)\n", sizeof(longLongType));
-  printf("Size of float: %ld byte(s)\n", sizeof(floatType));
+int main(void)
+{
+	char charType;
+	int intType;
+	float floatType;
+	long int longIntType;
+	long long int longLongType;
 
- 
-  return (0);
+	printf("Size of char: %ld byte(s)\n", sizeof(charType));
+	printf("Size of int: %ld bytes(s)\n", sizeof(intType));
+	printf("Size of long int: %li byte(s)\n", sizeof(longIntType));
+	printf("Size of long long int: %ld byte(s)\n", sizeof(longLongType));
+	printf("Size of float: %ld byte(s)\n", sizeof(floatType));
+
+	return (0);
 }
