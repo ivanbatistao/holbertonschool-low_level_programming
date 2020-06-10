c456d4391ed7706d679c97913fe39a9938da2441
Ivan E. Batista-Ochoa <1835@holbertonschool.com>
2020-06-10 20:02:45 +0000

    Mandatory task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
-int main()
+int main(void)
+
 {
-puts ("\"Programming is like building a multilingual puzzle");
-return (0);
+  /*This program print a the phares "Programming is like building a multilingual puzzle*/ 
+  puts("\"Programming is like building a multilingual puzzle");
+  return (0);
 }
