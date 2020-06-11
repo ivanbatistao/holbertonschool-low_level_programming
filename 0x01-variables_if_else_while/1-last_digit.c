5ec776811eea6f36ba4579f38ece17866e1833a1
Ivan E. Batista-Ochoa <1835@holbertonschool.com>
2020-06-11 22:52:05 +0000

    Mandatory task

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,15 +16,15 @@ int main(void)
 	printf("Last digit of %d is ", n);
 	if ((n % 10) > 5)
 	{
-	printf("is %d and is greater than 5\n", (n % 10));
+	printf("%d and is greater than 5\n", (n % 10));
 	}
 	else if (n == 0)
 	{
-	printf("is %d and is 0\n", (n % 10));
+	printf("%d and is 0\n", (n % 10));
 	}
 	else
 	{
-	printf("is %d and is less than 6 and not 0\n", (n % 10));
+	printf("%d and is less than 6 and not 0\n", (n % 10));
 	}
 
 	return (0);
