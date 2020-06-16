cab76126b5a40db88bace6c271109d446245c16f
Ivan E. Batista-Ochoa <1835@holbertonschool.com>
2020-06-16 13:37:25 +0000

    Mandatory task

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * main - prints alphabet
+ * using _putchar
+ *
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet(void)
+{
+	char n;
+
+	n = 'a';
+	while (n <= 'z')
+	{
+		_putchar(n);
+		n++;
+	}
+	_putchar('\n');
+
+}
