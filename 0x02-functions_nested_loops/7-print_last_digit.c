e4a3550dba22a455323f24dbdeca904229e92f3a
Ivan E. Batista-Ochoa <1835@holbertonschool.com>
2020-06-16 20:03:15 +0000

    Mandatory task

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,10 +1,10 @@
 #include "holberton.h"
 /**
- * int print_last_digit(int) -  prints the last digit of a number
+ * int print_last_digit -  prints the last digit of a number
  * @a: variable
  *
  *
- * Return: Always (1) (Success)
+ * Return: last digit of d
  */
 
 int print_last_digit(int a)
@@ -12,17 +12,17 @@ int print_last_digit(int a)
 	int b;
 
 	b = a % 10;
-	if (b >= 0)
+	if (a >= 0)
 	{
 		_putchar(b + '0');
-		return (1);
+		return (b);
 	}
 
 	else
         {
                 b = b * -1;
                 _putchar(b + '0');
-		return (1);
+		return (b);
 
 	}
 }
