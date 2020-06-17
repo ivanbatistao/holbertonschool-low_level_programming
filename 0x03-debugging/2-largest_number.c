57e980d543fc74e207c70073c77af39bb15a505d
Ivan E. Batista-Ochoa <1835@holbertonschool.com>
2020-06-17 02:51:21 +0000

    Mandatory task

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -15,15 +15,17 @@ int largest_number(int a, int b, int c)
 	if (a > b && a > c)
 	{
 		largest = a;
+		return (largest);
 	}
 	else if (b > a && b > c)
 	{
 		largest = b;
+		return (largest);
 	}
-	else if (c > a && c > b)
+	else
 	{
 		largest = c;
+		return (largest);
 	}
 
-	return (largest);
 }
