26692c3a2d7876bcef064eadc59716e4dad7a49f
Ivan E. Batista-Ochoa <1835@holbertonschool.com>
2020-06-18 03:53:52 +0000

    Mandatory task

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,43 @@
+#include "holberton.h"
+
+/**
+ * print_to_98 - prints all natural numbers from n to 98, followed by a new line
+ * @n: variable
+ *
+ * Return: Always 0.
+ */
+
+void print_to_98(int n)
+{
+
+	while (n < 98)
+	{
+
+		if (n != 98)
+		{
+			printf("%d, ", n);
+		}
+
+		else
+		{
+			printf("%d\n", n);
+		}
+		n++;
+	}
+
+	while (n >= 98)
+	{
+		if (n != 98)
+		{
+			printf("%d, ", n);
+		}
+
+		else
+		{
+			printf("%d\n", n);
+		}
+
+		n--;
+	}
+
+}
