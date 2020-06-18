bde58522053d9b95eb26f9e2ee7001959ec9d319
ivanbatistao <batistaochoaivan@gmail.com>
2020-06-18 21:43:11 +0000

    Mandatory task

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+
+/**
+ * more_numbers - prints 10 times the numbers, from 0 to 14
+ * Return: (0)
+ *
+ */
+
+void more_numbers(void)
+{
+	int n, l;
+
+	n = 0;
+
+	while (n <= 10)
+	{
+		l = 0;
+		while (l <= 14)
+		{
+			if (l >= 10)
+			{
+				_putchar(l / 10 + '0');
+			}
+
+			_putchar(l % 10 + '0');
+			l++;
+		}
+		putchar('\n');
+		n++;
+	}
+
+}
