ed67106404c884ab1a9c46891e96c658543dc77b
ivanbatistao <batistaochoaivan@gmail.com>
2020-06-19 01:00:09 +0000

    Mandatory task

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -3,25 +3,31 @@
 /**
  * print_diagonal - draws a diagonal line on the terminal
  * @n: variable
- *
+ * Return: (int)
  *
  */
 
 void print_diagonal(int n)
 {
-	if (n <= 0)
-	{
-		_putchar('\n');
-	}
-
-	else
-	{
-		while (n > 0)
-		{
-			_putchar(' ');
-			n--;
-		}
-		_putchar(' ');
-	}
-
+        if (n <= 0)
+        {
+                _putchar('\n');
+        }
+        else
+        {
+                int m, o;
+                m = 0;
+                while (m < n)
+                {
+                        o = 0;
+                        while (o < m)
+                        {
+                                _putchar(' ');
+                                o++;
+			}
+                        _putchar(92);
+                        _putchar('\n');
+                        m++;
+                }
+        }
 }
