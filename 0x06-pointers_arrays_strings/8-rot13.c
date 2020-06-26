5481e62bd41589460e03f1af62b1b787651e29b7
ivanbatistao <batistaochoaivan@gmail.com>
2020-06-26 04:19:17 +0000

    function that encodes a string using rot13

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+
+/**
+ * leet - function that encodes a string into 1337
+ * @s: pointer
+ * Return: p - pointer to s
+ *
+ */
+
+char *rot13(char *s)
+{
+	int i;
+	int j;
+	char *p = s;
+
+	char letters[] = "ABCDEFGHIJKLMNOPQRSTUWXYZabcdefghijklmnopqrstuwxyz";
+	char num[] = "NOPQRSTUVWXYZABCDEFGHJKLMnopqrstuvwxyzabcdefghjklm";
+
+	i = 0;
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		j = 0;
+		while (letters[j] != '\0')
+		{
+			if (s[i] == letters[j])
+			{
+				s[i] = num[j];
+				break;
+			}
+			j++;
+		}
+	}
+	return (p);
+}
